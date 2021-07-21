d93a7ddd891168c642c5ac21f20a257d65a6a3f5
Evan B <evanb96@gmail.com>
2021-07-21 11:49:20 -0500

    Add Fibers

---
+++ oqocs.items/Materials/Fiber.cs
@@ -0,0 +1,21 @@
+﻿namespace oqocs.items.materials
+{
+    public class Fiber : BasicItem
+    {
+        public Fiber(string name, string description, decimal value, int durability, Unit unit)
+        {
+            Name = name;
+            Description = description;
+            CostInPence = value;
+            Durability = durability;
+            Unit = unit;
+            Quality = Grade.C;
+        }
+
+        public static Fiber AlpacaFiber => new Fiber("Alpaca Fiber", "", 4, 2, Unit.Pounds);
+
+        public static Fiber CottonFiber => new Fiber("Cotton Fiber", "", 3, 1, Unit.Pounds);
+
+        public static Fiber PlantFiber => new Fiber("Plant Fiber", "", 1, 3, Unit.Pounds);
+    }
+}
---
+++ oqocs.items/Recipes/Tailoring.cs
@@ -0,0 +1,20 @@
+﻿using oqocs.items.materials;
+using System.Collections.Generic;
+
+namespace oqocs.items.Recipes
+{
+    public static class Tailoring
+    {
+        public static BasicRecipe SpoolOfAlpacaThread => new BasicRecipe()
+        {
+            Product = "Spool of Alpaca Thread",
+            DifficultyValue = 23,
+            PriceMultiplier = 3,
+            DurabilityMultiplier = 2,
+            Components = new List<RecipeComponent>()
+            {
+                new RecipeComponent(ComponentType.Unique, 1, Fiber.AlpacaFiber),
+            }
+        };
+    }
+}
