f911cc4d0f34ae5231a5f0bf5923e21c4a27f17f
Evan B <evanb96@gmail.com>
2021-07-29 00:26:46 -0500

    Project Consolidation
    pt 2

--- oqocs.items/recipes/Carpentry.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace oqocs.items.recipes
-{
-    public static class Carpentry
-    {
-        public static List<BasicRecipe> Furniture => carpentry.Furniture.All;
-    }
-}
