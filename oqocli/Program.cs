e526a1c63f5a8d4c72bbc29dc8b4552d2b034f01
Evan B <evanb96@gmail.com>
2021-08-01 16:05:54 -0500

    Update Program.cs

--- oqocli/Program.cs
+++ oqocli/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace oqocs
 {
@@ -6,6 +7,15 @@ namespace oqocs
     {
         private static void Main(string[] args)
         {
+            for (int i = 0; i < 10; i++)
+            {
+                Directory.CreateDirectory("output");
+                var fs = File.OpenWrite($"output\\{i}.txt");
+                var write = new StreamWriter(fs);
+                write.Write(CharacterManager.GenerateCharacter());
+                write.Flush();
+                write.Close();
+            }
             Console.Write(CharacterManager.GenerateCharacter());
             Console.ReadKey();
         }
