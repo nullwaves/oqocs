f25c457b028f544828676d7f7332383d919cce41
Evan B <evanb96@gmail.com>
2021-08-05 17:33:31 -0500

    Create Profession.cs

---
+++ oqocs/character/Profession.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace oqocs.character
+{
+    public class Profession
+    {
+        public string Name { get; set; }
+        public SkillBonus[] PrioritySkills { get; set; }
+
+        public Profession(string name, SkillBonus[] skills)
+        {
+            Name = name;
+            PrioritySkills = skills;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
