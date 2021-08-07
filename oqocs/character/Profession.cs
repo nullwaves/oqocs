32ee84ba3c6d1ab2edc58fc58973276b41fb3822
Evan B <evanb96@gmail.com>
2021-08-06 22:19:32 -0500

    Update Profession.cs

--- oqocs/character/Profession.cs
+++ oqocs/character/Profession.cs
@@ -23,12 +23,29 @@ namespace oqocs.character
             new Dictionary<int, SkillBonus[]>
             {
                 { 1, new SkillBonus[] { DefaultSkills.BaseQuality } },
-                { 2, new SkillBonus[] { DefaultSkills.BaseRate, } },
+                { 2, new SkillBonus[] { DefaultSkills.BaseRate, DefaultSkills.LoggingRate, DefaultSkills.MiningRate, } },
                 { 3, new SkillBonus[] { DefaultSkills.MiningQuality, } },
                 { 4, new SkillBonus[] { DefaultSkills.FurnitureQuality, DefaultSkills.FurnitureRate, DefaultSkills.UpholsteryQuality, DefaultSkills.UpholsteryRate,
                                         DefaultSkills.CabinetryQuality, DefaultSkills.CabinetryRate, DefaultSkills.SurfacesQuality, DefaultSkills.SurfacesRate,
                                         DefaultSkills.MasonryQuality, DefaultSkills.MasonryRate, DefaultSkills.HousingQuality, DefaultSkills.HousingRate,
                                         DefaultSkills.ImprovisedQuality, DefaultSkills.ImprovisedRate, } },
+            });
+
+        public static Profession Fisherman => new Profession(
+            "Fisherman",
+            new Dictionary<int, SkillBonus[]>
+            {
+                { 1, new SkillBonus[] { DefaultSkills.RodFishing, DefaultSkills.NetFishing, DefaultSkills.TrapFishing, } },
+                { 2, new SkillBonus[] { DefaultSkills.GatherFood, DefaultSkills.GatherFirewood, } },
+                { 3, new SkillBonus[] { DefaultSkills.SmallGameTracking, DefaultSkills.MediumGameTracking, } },
+            });
+
+        public static Profession Miner => new Profession(
+            "Miner",
+            new Dictionary<int, SkillBonus[]>
+            {
+                { 1, new SkillBonus[] { DefaultSkills.MiningRate, DefaultSkills.MiningQuality, } },
+                { 2, new SkillBonus[] { DefaultSkills.HandAxes, } },
             });
     }
 }
