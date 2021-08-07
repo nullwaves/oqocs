fea785a78a729f0085a49c19029379371750e555
Evan B <evanb96@gmail.com>
2021-08-06 22:09:46 -0500

    Update Profession.cs

--- oqocs/character/Profession.cs
+++ oqocs/character/Profession.cs
@@ -1,11 +1,13 @@
-﻿namespace oqocs.character
+﻿using System.Collections.Generic;
+
+namespace oqocs.character
 {
     public class Profession
     {
         public string Name { get; set; }
-        public SkillBonus[] PrioritySkills { get; set; }
+        public Dictionary<int, SkillBonus[]> PrioritySkills { get; set; }
 
-        public Profession(string name, SkillBonus[] skills)
+        public Profession(string name, Dictionary<int, SkillBonus[]> skills)
         {
             Name = name;
             PrioritySkills = skills;
@@ -15,5 +17,18 @@
         {
             return Name;
         }
+
+        public static Profession Laborer => new Profession(
+            "Laborer",
+            new Dictionary<int, SkillBonus[]>
+            {
+                { 1, new SkillBonus[] { DefaultSkills.BaseQuality } },
+                { 2, new SkillBonus[] { DefaultSkills.BaseRate, } },
+                { 3, new SkillBonus[] { DefaultSkills.MiningQuality, } },
+                { 4, new SkillBonus[] { DefaultSkills.FurnitureQuality, DefaultSkills.FurnitureRate, DefaultSkills.UpholsteryQuality, DefaultSkills.UpholsteryRate,
+                                        DefaultSkills.CabinetryQuality, DefaultSkills.CabinetryRate, DefaultSkills.SurfacesQuality, DefaultSkills.SurfacesRate,
+                                        DefaultSkills.MasonryQuality, DefaultSkills.MasonryRate, DefaultSkills.HousingQuality, DefaultSkills.HousingRate,
+                                        DefaultSkills.ImprovisedQuality, DefaultSkills.ImprovisedRate, } },
+            });
     }
 }
